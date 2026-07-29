15a2426f97787761d14847ee9b41642f752f8af8
Mizerex <130624316+Mizerex@users.noreply.github.com>
2026-07-29 20:07:35 -0300

    feat: rebuild tonearm with realistic mechanical details

--- components/Tonearm.tsx
+++ components/Tonearm.tsx
@@ -1,6 +1,33 @@
 "use client";
+
 import { useTonearmProgress } from "@/hooks/useTonearmProgress";
-export function Tonearm({ position, duration, hasTrack }: { position: number; duration: number; hasTrack: boolean }) {
+import styles from "./Tonearm.module.css";
+
+type TonearmProps = {
+  position: number;
+  duration: number;
+  hasTrack: boolean;
+  playing: boolean;
+};
+
+export function Tonearm({ position, duration, hasTrack, playing }: TonearmProps) {
   const angle = useTonearmProgress(position, duration, hasTrack);
-  return <div className="tonearm-assembly"><div className="tonearm-base"><i /></div><div className="tonearm" style={{ transform: `rotate(${angle}deg)` }}><div className="counterweight" /><div className="arm-tube" /><div className="headshell"><i /></div></div><div className="arm-rest" /></div>;
+  const stateClass = hasTrack ? (playing ? styles.lowered : styles.parked) : styles.parked;
+
+  return (
+    <div className={`${styles.assembly} ${stateClass}`} aria-hidden="true">
+      <div className={styles.base} />
+      <div className={styles.arm} style={{ transform: `rotate(${angle}deg)` }}>
+        <div className={styles.counterweight} />
+        <div className={styles.pivotCollar} />
+        <div className={styles.tube} />
+        <div className={styles.headshell}>
+          <div className={styles.cartridge}>
+            <i className={styles.stylus} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.rest} />
+    </div>
+  );
 }
