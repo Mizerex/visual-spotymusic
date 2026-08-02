843be6b619f2c13c9d2fdd68ace11b261250762a
Mizerex <130624316+Mizerex@users.noreply.github.com>
2026-08-01 22:57:13 -0300

    feat: montar imagem completa da fita mobile

---
+++ app/mobile-concept/cassetteImage.ts
@@ -0,0 +1,6 @@
+import { cassettePart1 } from "./cassette.part1";
+import { cassettePart2 } from "./cassette.part2";
+import { cassettePart3 } from "./cassette.part3";
+import { cassettePart4 } from "./cassette.part4";
+
+export const cassetteImageSrc = `data:image/webp;base64,${cassettePart1}${cassettePart2}${cassettePart3}${cassettePart4}`;
